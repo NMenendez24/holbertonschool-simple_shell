f5a6d48e7177bafc59af203f53423096e947215b
NMenendez24 <nahuelmt24@gmail.com>
2022-12-09 16:42:07 -0300

    Betty Fix

--- simple_shell.c
+++ simple_shell.c
@@ -1,9 +1,8 @@
 #include "shell.h"
 
 /**
-  *
-  *
-  *
+  * main - Command interpreter
+  * Return: -1 on error
   */
 
 int main(void)
@@ -15,13 +14,10 @@ int main(void)
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO) == 1)
-			_puts("$ ");
+		(isatty(STDIN_FILENO) == 1) ? _puts("$ ") : (void) 0;
 		c = getline(&buff, &bufsize, stdin);
-		if (c == -1 && isatty(STDIN_FILENO) == 1)
-			free(buff), _putchar(10), exit(0);
-		else if (c == -1)
-			free(buff), exit(0);
+		(c == -1 && isatty(STDIN_FILENO) == 1) ? free(buff), _putchar(10), exit(0) :
+			(c == -1) ? free(buff), exit(0) : (void) 0;
 		buffdup = _strdup(buff), argcount = _argcounter(buffdup);
 		token = strtok(buffdup, " \\\t\n");
 		if (!token)
@@ -29,10 +25,10 @@ int main(void)
 			continue; }
 		av = malloc(sizeof(char *) * (argcount + 1));
 		if (!av)
-		{	free(buff), free(buffdup);
+		{	free(buff), free(buffdup), write(2, "Fatal error\n", 12);
 			return (-1); }
-		if (_strcmp(token, "exit") == 0)
-			free(buff), free(buffdup), free(av), exit(0);
+		(strcmp(token, "exit") == 0) ? free(buff), free(buffdup),
+			free(av), exit(0) : (void) 0;
 		if (_strcmp(token, "env") == 0)
 		{	_printenv(), free(av), free(buffdup);
 			continue; }
@@ -40,20 +36,14 @@ int main(void)
 			av[0] = token;
 		else
 		{	pathaux = _getenv("PATH"), com_path = _getcommand(pathaux, token);
-		free(pathaux - 5); 
+		free(pathaux - 5);
 			if (com_path == NULL)
-			{	/*free(buff),*/ free(buffdup), free(av);
-				perror("Command not found");
+			{	free(buffdup), free(av), write(2, "Command not found\n", 18);
 				continue; }
-			av[0] = com_path, token = strtok(buff, " \\\t\n"); }
+		av[0] = com_path, token = strtok(buff, " \\\t\n"); }
 		for (ac = 1; ac <= argcount; ac++)
 			aux = strtok(NULL, " \\\t\n"), av[ac] = aux;
 		pid_check = fork();
-		if (pid_check == 0)
-			execve(av[0], av, environ);
+		(pid_check == 0) ? execve(av[0], av, environ) : 0;
 		wait(&status);
-/*		if (isatty(STDIN_FILENO) == 1)
-			free(buffdup);*/
-		free(buffdup), free(av);
-		if (com_path)
-			free(com_path); } }
+		free(buffdup), free(av), (com_path) ? free(com_path) : (void) 0; }}
